2720a166a779d5b6dbf944b3ca48ef9adb8203f5
Arjun <niteshkrjhag@gmail.com>
2024-11-10 19:21:10 +0530

    Testing Done

--- Unscramble/app/build.gradle.kts
+++ Unscramble/app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
@@ -74,10 +74,17 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.1.1")
 
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    // Junit Testing
+    testImplementation("junit:junit:4.13.2")
+
+
 }
